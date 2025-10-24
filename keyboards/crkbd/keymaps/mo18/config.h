94d767dfffd7c1617cf4917500a1cc1dd15b75e2
Mario Scubla <mario@scubla.net>
2025-10-24 18:16:31 -0400

    wip: symbol layer changes

--- keyboards/crkbd/keymaps/mo18/config.h
+++ keyboards/crkbd/keymaps/mo18/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define QUICK_TAP_TERM 0
 #define TAPPING_TERM 200
-#define PERMISSIVE_HOLD // also see PERMISSIVE_HOLD_PER_KEY
+// #define PERMISSIVE_HOLD // also see PERMISSIVE_HOLD_PER_KEY
 // #define HOLD_ON_OTHER_KEY_PRESS // also see HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 // #define TAPPING_TERM_PER_KEY
 
